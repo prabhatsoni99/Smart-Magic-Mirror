000710819d8dad1f91d1e42bcb861f3b807df20b
Prabhat Soni <43891076+prabhatsoni99@users.noreply.github.com>
2019-02-01 23:49:16 +0530

    neated it out, wrote autostart and bashrc stuff

--- execute-me.sh
+++ execute-me.sh
@@ -3,14 +3,10 @@
 python3 MagicMirror.py
 
 
+#We used a Raspberry Pi & desired that the program should run automatically whenever the Raspberry Pi is switched on
+#To do this, you can run the following commands:-
+#echo cd ~/Desktop/Smart-Magic-Mirror >> ~/.bashrc
+#echo ./execute-me.sh >> ~/.bashrc
 
-#Check for dependencies in this script otherwise install. List of dependencies:-
-#sudo apt-get install python-imaging-tk
-#sudo pip3 install geocoder
-#pip3 install --upgrade google-api-python-client
-#pip3 install google_auth_oauthlib
-
-
-#Our program should run on bootup without any human interference
-#Eg. If power goes off. You on the device. then you have to type python3 <filename>. This should happen by itself on bootup
+#Alternatively, you can use autostart. See:-
 #https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup#method-2-autostart
