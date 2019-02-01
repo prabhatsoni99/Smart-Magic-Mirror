786479ba83387f1ff668cd51893cd32a770ee420
Prabhat Soni <43891076+prabhatsoni99@users.noreply.github.com>
2019-02-01 23:32:18 +0530

    Added all 4 files intially

---
+++ execute-me.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+python3 MagicMirror.py
+
+
+
+#Check for dependencies in this script otherwise install. List of dependencies:-
+#sudo apt-get install python-imaging-tk
+#sudo pip3 install geocoder
+#pip3 install --upgrade google-api-python-client
+#pip3 install google_auth_oauthlib
+
+
+#Our program should run on bootup without any human interference
+#Eg. If power goes off. You on the device. then you have to type python3 <filename>. This should happen by itself on bootup
+#https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup#method-2-autostart
---
+++ readme.txt
@@ -0,0 +1,8 @@
+References:-
+https://github.com/HackerShackOfficial/Smart-Mirror
+
+
+IED Project by - Prabhat, Kshitij, Anshul, Sargam
+Magic Mirror
+Software side - Anshul, Prabhat
+Hardware side - Sargam, Kshitij
